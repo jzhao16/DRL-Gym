d47633f0b1fe6c99e81db73655db952c3041d979
Jing Zhao <jing.zhao@careerbuilder.com>
2020-03-25 23:15:34 -0400

    ddpg

--- lunar_lander_DDPG/ddpg_compare.py
+++ lunar_lander_DDPG/ddpg_compare.py
@@ -259,7 +259,7 @@ def train(sess, env, actor, critic, actor_noise, buffer_size, min_batch, ep):
 
             # env.render()
 
-            action = actor.predict(np.reshape(state, (1, actor.s_dim))) +actor_noise()
+            action = actor.predict(np.reshape(state, (1, actor.s_dim))) 
             noise = actor_noise()
             # no noise
 	
